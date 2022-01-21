e5be6e5206b7a85f2b95e396d602a3856182ce4f
Bahtah <btolikov@gmail.com>
2022-01-21 21:38:57 +0300

    add class

---
+++ src/com/company/InfoTruck.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class InfoTruck {
+
+    private int id;
+    private String name;
+    private Driver driver;
+    private State state;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public static InfoTruck showInfo(int id, String name, Driver driver, State state) {
+        InfoTruck info = new InfoTruck();
+        info.id = id;
+        info.name = name;
+        info.driver = driver;
+        info.state = state;
+
+        return info;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,45 @@
 package com.company;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class Main {
 
+    public static final GsonBuilder Builder = new GsonBuilder();
+    public static final Gson gson = Builder.setPrettyPrinting().create();
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        Driver driver = new Driver("Djon", 45);
+        Driver driver2 = new Driver("Alex", 30);
+        Driver driver3 = new Driver("Arnold", 55);
+
+        InfoTruck[] trucks = {
+                InfoTruck.showInfo(1,"Хонда", driver, State.BASE),
+                InfoTruck.showInfo(2,"MAZ", driver2, State.ROUTE),
+                InfoTruck.showInfo(3,"Kamaz", driver3, State.REPAIR),
+        };
+
+        String allTrucks = gson.toJson(trucks);
+        writeInfoTRuck(allTrucks);
+        System.out.println(allTrucks);
+    }
+
+    private static void writeInfoTRuck(String obj) {
+        Path write = Paths.get("./trucks.json");
+        try {
+            Files.writeString(write,obj, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+        }catch (IIOException e) {
+            e.getMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
